0558985b5b5bbdf5f927559f5d668bd3aac07d7c
Alan Moran <bonzofenix@gmail.com>
2015-03-08 18:13:35 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # go-research
+
+## Useful resources
+
+[Deploying a go webapp on cf](http://blog.anynines.com/how-to-deploy-a-go-app-on-anynines-and-cloud-foundry/)
---
+++ gobyexample/values/values.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + "golang")
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+	// gogolang
+	// 1+1 = 2
+	// 7.0/3.0 = 2.3333333333333335
+	// false
+	// true
+	// false
+
+}
